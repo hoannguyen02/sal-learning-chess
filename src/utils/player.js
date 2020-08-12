ae9a1bfd2d94bbefea9d8aeaad07ba96c5c85919
hoannguyen02 <nguyenvanhoan02@gmail.com>
2020-08-12 16:58:32 +0700

    Clean up: Move generate piece/pieces into player

--- src/utils/player.js
+++ src/utils/player.js
@@ -1,8 +1,32 @@
-import { generatePiecesForPlayer } from './generate-pieces-for-player';
+import { PlayerName, BLACK_PIECES, WHITE_PIECES } from '../constants';
 
-export function initialPlayer(name) {
+export { initialPlayer };
+
+function initialPlayer(name) {
   return {
     name,
     pieces: generatePiecesForPlayer(name),
+  };
+}
+
+function generatePiecesForPlayer(name) {
+  if (name === PlayerName.BLACK) {
+    return BLACK_PIECES.map(({ position, type }) =>
+      generatePiece(position, type)
+    );
+  }
+
+  return WHITE_PIECES.map(({ position, type }) =>
+    generatePiece(position, type)
+  );
+}
+
+function generatePiece(pos, type) {
+  return {
+    pos,
+    type,
+    move: function name(nextPos) {
+      console.log(`${type} at ${pos[0][1]} moving to ${nextPos[0][1]}`);
+    },
   };
 }
